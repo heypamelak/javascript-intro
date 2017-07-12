6574cfb0b0b0bdc61f630499fac2946fd8bc6fb7
Pamela Krikorian <heypamelak@gmail.com>
2017-07-11 20:17:18 -0500

    Added js

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+window.onload = function(event) {
+  console.log('Hi, Mom!');
+  console.log(document);
+  let ul = document.createElement('ul');
+  console.log(ul);
+
+  document.getElementById('list').innerHTML = 'I like cats.';
+
+
+
+}
